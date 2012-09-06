fb0c62400f9cd5ed19a464ecce8921f4940dd271
Knut Kristian Johansen <knutkj@gmail.com>
2012-09-06 19:52:42 +0200

    Build script now builds 32 bit version of PowerShell module.

--- build.ps1
+++ build.ps1
@@ -51,15 +51,35 @@ $assembly = $projectPath | Join-Path -ChildPath bin\debug\DotNetDetector.dll
 $wixFile, $manifest, $module, $assembly | Copy-Item -Destination $tmpPath
 
 #
+# Module x86 preparation.
+#
+$wixFileContent = $tmpPath |
+    Join-Path -ChildPath (Split-Path -Leaf $wixFile) |
+    Get-Item |
+    Get-Content
+$wixXmlDoc = [xml] $wixFileContent
+$wixXmlDoc.Wix.Product.Name = 'DotNetDetector PowerShell Module (x86)'
+$wixXmlDoc.Wix.Product.UpgradeCode = '58E7A7FF-E5CF-4BDC-A7B9-12E1657357FB'
+$wixXmlDoc.Wix.Product.Package.Platform = 'x86'
+$wixXmlDoc.Wix.Product.Directory.Directory.Id = 'SystemFolder'
+$wixXmlDoc.Wix.Product.DirectoryRef.Component | % { $_.Win64 = 'no' }
+$wixXmlDoc.Save(($tmpPath | Join-Path -ChildPath Productx86.wxs))
+
+#
 # Module build.
 #
 Push-Location -Path $tmpPath
-& $candle Product.wxs
-& $light Product.wixobj
+'Product.wxs', 'Productx86.wxs' | ForEach-Object -Process { & $candle $_ }
+'Product.wixobj', 'Productx86.wixobj' | ForEach-Object -Process { & $light $_ }
 $moduleVersion = ($assembly | Get-ItemProperty).VersionInfo.ProductVersion
-$moduleFileName = "DotNetDetector-$moduleVersion-x64.msi"
+$module32FileName = "DotNetDetectorPowerShellModule-$moduleVersion-x86.msi"
+$module64FileName = "DotNetDetectorPowerShellModule-$moduleVersion-x64.msi"
 'Product.msi' |
     Copy-Item -Destination (
-        $modulePath | Join-Path -ChildPath $moduleFileName
+        $modulePath | Join-Path -ChildPath $module64FileName
+    )
+'Productx86.msi' |
+    Copy-Item -Destination (
+        $modulePath | Join-Path -ChildPath $module32FileName
     )
 Pop-Location
